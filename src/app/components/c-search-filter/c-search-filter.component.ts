ff5694df7f20935ec5157e38af390d59fee58d2c
fernando-hidalgo <fernan.hidalgo02@gmail.com>
2023-03-15 23:30:09 +0100

    feat: Search View

--- src/app/components/c-search-filter/c-search-filter.component.ts
+++ src/app/components/c-search-filter/c-search-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ArtworkService } from 'src/app/services/artwork.service';
 
 @Component({
   selector: 'app-c-search-filter',
@@ -6,10 +7,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./c-search-filter.component.scss']
 })
 export class CSearchFilterComponent implements OnInit {
+  @Input() searchResult;
+  @Input() filter;
+  @Input() type;
+  @Output("updateData") updateData: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  timeout = null;
+  showOptions = false;
+  optionsX = 0
+  optionsY = 0
+  placeholder;
+
+  constructor(private artworkService: ArtworkService) { }
 
   ngOnInit(): void {
+    this.setPlaceholder()
+  }
+
+  setPlaceholder() {
+    if (this.type === 'nameFilter') this.placeholder = "Nombre..."
+    if (this.type === 'artistFilter') this.placeholder = "Artista..."
+    if (this.type === 'styleFilter') this.placeholder = "Estilo..."
+    if (this.type === 'museumFilter') this.placeholder = "Museo..."
+  }
+
+  triggerFilterOptions(event) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.filterLimitOptions(event.target.value);
+    }, 600);
+  }
+
+  filterLimitOptions(text){
+    //Copia de los datos, para aplicarles cambios sin perder el original
+    let filterAllOptions: string[] = this.searchResult[this.type];
+    this.filter = filterAllOptions.filter(str => str.toLowerCase().includes(text.toLowerCase()));
+  }
+
+  filterSetOption(option){
+    (document.getElementById(this.type) as HTMLInputElement).value = option;
+    document.getElementById(this.type).style.pointerEvents = 'none';
+    this.findFiltered()
+  }
+
+  findFiltered() {
+    let params = {};
+
+    if((document.getElementById('nameFilter') as HTMLInputElement).value != ""){
+      params['nameFilter'] = (document.getElementById('nameFilter') as HTMLInputElement).value;
+    }
+    
+    if((document.getElementById('artistFilter') as HTMLInputElement).value != ""){
+      params['artistFilter'] = (document.getElementById('artistFilter') as HTMLInputElement).value;
+    }
+
+    if((document.getElementById('styleFilter') as HTMLInputElement).value != ""){
+      params['styleFilter'] = (document.getElementById('styleFilter') as HTMLInputElement).value;
+    }
+
+    if((document.getElementById('museumFilter') as HTMLInputElement).value != ""){
+      params['museumFilter'] = (document.getElementById('museumFilter') as HTMLInputElement).value;
+    }
+
+    this.artworkService.findFiltered(params).subscribe(data => {
+      this.updateData.emit({data});
+    });
+  }
+
+  onFocus(){
+    this.showOptions = true;
+  }
+
+  onBlur(){
+    //Blur rápido, cuando se clicka FUERA del dropdown
+    if(this.optionsX < 3 || this.optionsX > 290 || this.optionsY < 3 || this.optionsY > 160){
+      this.showOptions = false;
+    } else {
+      //Blur lento, cuando se clicka DENTRO del dropdown (Permite que de tiempo a seleccionar una opción)
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        this.showOptions = false;
+      }, 100);
+    }
+  }
+
+  dropdownBounds(event) {
+    let bounds = document.getElementById(`${this.type}-dropdown`).getBoundingClientRect();
+    this.optionsX = Math.max(0, event.clientX - bounds.left);
+    this.optionsY = Math.max(0, event.clientY - bounds.top);
   }
 
 }
--- src/app/views/v-home-search/v-home-search.component.ts
+++ src/app/views/v-home-search/v-home-search.component.ts
@@ -14,29 +14,10 @@ export class VHomeSearchComponent implements OnInit {
   page: number = 1;
   timeout = null;
 
-  //NOMBRE
   nameFilter: string[] = [];
-  showNameOptions = false;
-  nameOptionsX = 0
-  nameOptionsY = 0
-
-  //ARTISTA
   artistFilter: string[] = [];
-  showArtistOptions = false;
-  artistOptionsX = 0
-  artistOptionsY = 0
-
-  //ESTILO
   styleFilter: string[] = [];
-  showNameStyle = false;
-  styleOptionsX = 0
-  styleOptionsY = 0
-
-  //MUSEO
   museumFilter: string[] = [];
-  showNameMuseum = false;
-  museumOptionsX = 0
-  museumOptionsY = 0
 
   userRatings = [];
 
@@ -52,96 +33,16 @@ export class VHomeSearchComponent implements OnInit {
     });
   }
 
-  triggerFilterOptions(event, filterType) {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      this.filterLimitOptions(event.target.value, filterType);
-    }, 600);
-  }
-
-  filterLimitOptions(text, type){
-    //Copia de los datos, para aplicarles cambios sin perder el original
-    let filterAllOptions: string[] = this.searchResult[type];
-
-    if(type === 'nameFilter'){
-      this.nameFilter = filterAllOptions.filter(str => str.toLowerCase().includes(text.toLowerCase()));
-    }
-
-    if(type === 'artistFilter'){
-      this.artistFilter = filterAllOptions.filter(str => str.toLowerCase().includes(text.toLowerCase()));
-    }
-
-    if(type === 'styleFilter'){
-      this.styleFilter = filterAllOptions.filter(str => str.toLowerCase().includes(text.toLowerCase()));
-    }
-
-    if(type === 'museumFilter'){
-      this.museumFilter = filterAllOptions.filter(str => str.toLowerCase().includes(text.toLowerCase()));
-    }
-  }
-
-  filterSetOption(option, filterId){
-    (document.getElementById(filterId) as HTMLInputElement).value = option;
-    document.getElementById(filterId).style.pointerEvents = 'none';
-    //document.getElementById(`${filterId}-dropdown`).style.display = 'none'
-
-    this.findFiltered()
-  }
-
-  findFiltered() {
-    let params = {};
-
-    if((document.getElementById('nameFilter') as HTMLInputElement).value != ""){
-      params['nameFilter'] = (document.getElementById('nameFilter') as HTMLInputElement).value;
-    }
-
-    /*
-    if((document.getElementById('artist-filter') as HTMLInputElement).value != ""){
-      options['artistFilter'] = (document.getElementById('name-filter') as HTMLInputElement).value;
-    }
-
-    if((document.getElementById('style-filter') as HTMLInputElement).value != ""){
-      options['styleFilter'] = (document.getElementById('name-filter') as HTMLInputElement).value;
-    }
-
-    if((document.getElementById('museum-filter') as HTMLInputElement).value != ""){
-      options['museumFilter'] = (document.getElementById('name-filter') as HTMLInputElement).value;
-    }
-    */
-
-    this.artworkService.findFiltered(params).subscribe(data => {
-      this.loadData(data);
-    });
+  updateData(event){
+    this.loadData(event.data)
   }
 
   loadData(data){
     this.searchResult = data;
     this.artworks = data.artworks;
-
     this.nameFilter = data.nameFilter;
-  }
-
-  onFocus(){
-    this.showNameOptions = true;
-  }
-  
-  onBlur(){
-    //Blur rápido, cuando se clicka FUERA del dropdown
-    if(this.nameOptionsX < 3 || this.nameOptionsX > 290 || this.nameOptionsY < 3 || this.nameOptionsY > 160){
-      this.showNameOptions = false;
-    } else {
-      //Blur lento, cuando se clicka DENTRO del dropdown
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(() => {
-        this.showNameOptions = false;
-      }, 100);
-    }
-    
-  }
-
-  changeStarsWidth(event) {
-    let bounds = document.getElementById("nameFilter-dropdown").getBoundingClientRect();
-    this.nameOptionsX = Math.max(0, event.clientX - bounds.left);
-    this.nameOptionsY = Math.max(0, event.clientY - bounds.top);
+    this.artistFilter = data.artistFilter;
+    this.styleFilter = data.styleFilter;
+    this.museumFilter = data.museumFilter;
   }
 }
