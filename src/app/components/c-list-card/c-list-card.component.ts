e27a8da636e0a52fdad233f53a474be716d4f9f1
fernando-hidalgo <fernan.hidalgo02@gmail.com>
2023-06-04 21:25:43 +0200

    fix: Changes related to Lists CSS

--- src/app/components/c-list-card/c-list-card.component.ts
+++ src/app/components/c-list-card/c-list-card.component.ts
@@ -10,10 +10,16 @@ import { ArtlistService } from 'src/app/services/artlist.service';
 export class CListCardComponent implements OnInit {
   @Input() data;
   cover: string;
+  listName: string
+  listDescription: string
 
   constructor(private artlistService: ArtlistService) { }
 
   ngOnInit(): void {
+    const { name, text } = this.data;
+    this.listDescription = text.replace(/\n/g, '<br>');
+    this.listName = name;
+
     this.artlistService.getCover(this.data.id).subscribe((signedURL) => {
       this.cover = signedURL
     })
