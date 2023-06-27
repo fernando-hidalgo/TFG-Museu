43ab589a1d2623f1e889125f85fd990b3a3c6394
fernando-hidalgo <fernan.hidalgo02@gmail.com>
2023-06-27 16:49:15 +0200

    chore: deployed host for exposition day

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 /*ENVIRONMENTS*/
 //DEV
-export const HOST='http://localhost:3000'
+//export const HOST='http://localhost:3000'
 
 //PRO
-//export const HOST='https://tfg-museu-back-production.up.railway.app'
+export const HOST='https://tfg-museu-back-production.up.railway.app'
